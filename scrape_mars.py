cde4968118cbf0e02967f6037a5558abf0b406af
searobea <searobea@gmail.com>
2019-05-11 13:55:22 -0500

    update

--- scrape_mars.py
+++ scrape_mars.py
@@ -7,7 +7,7 @@ import time
 
 
 def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
+    #Replace the path with your actual path to the chromedriver
     executable_path = {"executable_path": "chromedriver.exe"}
     return Browser("chrome", **executable_path, headless=False)
 
